bfec4973415a7fa131f77ad0ffa6baab5f1f77dc
Joshua <joshuaterrones18@icloud.com>
2025-04-07 12:09:02 -0500

    First Version

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Asegura que vea todos tus archivos dentro de src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
